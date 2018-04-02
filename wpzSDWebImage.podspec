e162072e4e211d34e5ff2113771401ccca678508
王培铮 <wangpeizheng@uddtrip.com>
2018-04-02 17:14:42 +0800

    placehoder
    Signed-off-by: 王培铮 <wangpeizheng@uddtrip.com>

--- wpzSDWebImage.podspec
+++ wpzSDWebImage.podspec
@@ -9,7 +9,7 @@
 
 Pod::Spec.new do |s|
 s.name             = 'wpzSDWebImage'
-s.version          = '0.1.1'
+s.version          = '0.1.2'
 s.summary          = 'wpzSDWebImage For SDWebImage Clude Some ImagePlaceHoder For ImageView.'
 
 # This description is used to generate tags and improve search results.
--- wpzSDWebImage/Classes/SDWebImage/UIImageView+WebCache.m
+++ wpzSDWebImage/Classes/SDWebImage/UIImageView+WebCache.m
@@ -6,76 +6,43 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 #import "UIImageView+WebCache.h"
 #import "objc/runtime.h"
 #import "UIView+WebCacheOperation.h"
-
 static char imageURLKey;
 static char TAG_ACTIVITY_INDICATOR;
 static char TAG_ACTIVITY_STYLE;
 static char TAG_ACTIVITY_SHOW;
-
 @implementation UIImageView (WebCache)
-
 - (void)sd_setImageWithURL:(NSURL *)url {
     
     [self sd_setImageWithURL:url placeholderImage:nil options:0 progress:nil completed:nil];
     
 }
-
 - (void)sd_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:0 progress:nil completed:nil];
 }
-
 - (void)sd_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:options progress:nil completed:nil];
 }
-
 - (void)sd_setImageWithURL:(NSURL *)url completed:(SDWebImageCompletionBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:nil options:0 progress:nil completed:completedBlock];
 }
-
 - (void)sd_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder completed:(SDWebImageCompletionBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:0 progress:nil completed:completedBlock];
 }
-
 - (void)sd_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options completed:(SDWebImageCompletionBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:options progress:nil completed:completedBlock];
 }
-
 - (void)sd_setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options progress:(SDWebImageDownloaderProgressBlock)progressBlock completed:(SDWebImageCompletionBlock)completedBlock {
     [self sd_cancelCurrentImageLoad];
     objc_setAssociatedObject(self, &imageURLKey, url, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    
-    if (!(options & SDWebImageDelayPlaceholder)) {
-        dispatch_main_async_safe(^{
-            
-            ////            if (placeholder) {
-            ////
-            ////                self.image = placeholder;
-            ////            }else{
-            ////
-            ////                self.image = [UIImage imageNamed:@"Ticket.bundle/image_placeholder"];
-            ////            }
-            //            self.contentMode = UIViewContentModeCenter;
-            //            self.clipsToBounds = YES;
-            //            self.image = [UIImage imageNamed:@"Ticket.bundle/image_placeholder"];
-        });
-    }
-    
-    
-    
-    if (url && [[url absoluteString] length] > 0) {
+#pragma mark 有url开始 加载   加载前先显示占位图
+    if (url  && [[url absoluteString] length] > 0) {
         
-        // check if activityView is enabled or not
-        //        if ([self showActivityIndicatorView]) {
-        //            [self addActivityIndicator];
-        //        }
         
         [self addActivityIndicator];
         self.image = nil;
-        
         self.backgroundColor = [UIColor colorWithRed:((float)((0xf5f5f5 & 0xFF0000) >> 16))/255.0 green:((float)((0xf5f5f5 & 0xFF00) >> 8))/255.0 blue:((float)(0xf5f5f5 & 0xFF))/255.0 alpha:1.0];
         
         __weak __typeof(self)wself = self;
@@ -83,6 +50,7 @@ static char TAG_ACTIVITY_SHOW;
             
             if (!wself){
                 //wangpeizheng
+                //                self.image = [UIImage imageNamed:@"PlaecHoderImage.bundle/image_failed"];
                 [[wself activityIndicator] setImage:[UIImage imageNamed:@"PlaecHoderImage.bundle/image_failed"]];
                 return;
             }
@@ -115,12 +83,22 @@ static char TAG_ACTIVITY_SHOW;
         }];
         [self sd_setImageLoadOperation:operation forKey:@"UIImageViewImageLoad"];
     } else {
+        //有占位图,就显示占位图
+        if (placeholder) {
+            self.backgroundColor = [UIColor whiteColor];
+            [self removeActivityIndicator];
+            self.image = placeholder;
+            self.contentMode = UIViewContentModeScaleToFill;
+            return ;
+        }
+        
         dispatch_main_async_safe(^{
             self.image = nil;
             self.backgroundColor = [UIColor colorWithRed:((float)((0xf5f5f5 & 0xFF0000) >> 16))/255.0 green:((float)((0xf5f5f5 & 0xFF00) >> 8))/255.0 blue:((float)(0xf5f5f5 & 0xFF))/255.0 alpha:1.0];
             [self addActivityIndicator];
-            //wangpeizheng
+            //            wangpeizheng
             [[self activityIndicator] setImage:[UIImage imageNamed:@"PlaecHoderImage.bundle/image_failed"]];
+            //            self.image = [UIImage imageNamed:@"PlaecHoderImage.bundle/image_failed"];
             if (completedBlock) {
                 NSError *error = [NSError errorWithDomain:SDWebImageErrorDomain code:-1 userInfo:@{NSLocalizedDescriptionKey : @"Trying to load a nil url"}];
                 completedBlock(nil, error, SDImageCacheTypeNone, url);
@@ -128,18 +106,15 @@ static char TAG_ACTIVITY_SHOW;
         });
     }
 }
-
 - (void)sd_setImageWithPreviousCachedImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options progress:(SDWebImageDownloaderProgressBlock)progressBlock completed:(SDWebImageCompletionBlock)completedBlock {
     NSString *key = [[SDWebImageManager sharedManager] cacheKeyForURL:url];
     UIImage *lastPreviousCachedImage = [[SDImageCache sharedImageCache] imageFromDiskCacheForKey:key];
     
     [self sd_setImageWithURL:url placeholderImage:lastPreviousCachedImage ?: placeholder options:options progress:progressBlock completed:completedBlock];
 }
-
 - (NSURL *)sd_imageURL {
     return objc_getAssociatedObject(self, &imageURLKey);
 }
-
 - (void)sd_setAnimationImagesWithURLs:(NSArray *)arrayOfURLs {
     [self sd_cancelCurrentAnimationImagesLoad];
     __weak __typeof(self)wself = self;
@@ -170,82 +145,61 @@ static char TAG_ACTIVITY_SHOW;
     
     [self sd_setImageLoadOperation:[NSArray arrayWithArray:operationsArray] forKey:@"UIImageViewAnimationImages"];
 }
-
 - (void)sd_cancelCurrentImageLoad {
     [self sd_cancelImageLoadOperationWithKey:@"UIImageViewImageLoad"];
 }
-
 - (void)sd_cancelCurrentAnimationImagesLoad {
     [self sd_cancelImageLoadOperationWithKey:@"UIImageViewAnimationImages"];
 }
-
-
 #pragma mark -
 - (UIImageView *)activityIndicator {
     return (UIImageView *)objc_getAssociatedObject(self, &TAG_ACTIVITY_INDICATOR);
 }
-
 - (void)setActivityIndicator:(UIActivityIndicatorView *)activityIndicator {
     objc_setAssociatedObject(self, &TAG_ACTIVITY_INDICATOR, activityIndicator, OBJC_ASSOCIATION_RETAIN);
 }
-
 - (void)setShowActivityIndicatorView:(BOOL)show{
     objc_setAssociatedObject(self, &TAG_ACTIVITY_SHOW, [NSNumber numberWithBool:show], OBJC_ASSOCIATION_RETAIN);
 }
-
 - (BOOL)showActivityIndicatorView{
     return [objc_getAssociatedObject(self, &TAG_ACTIVITY_SHOW) boolValue];
 }
-
 - (void)setIndicatorStyle:(UIActivityIndicatorViewStyle)style{
     objc_setAssociatedObject(self, &TAG_ACTIVITY_STYLE, [NSNumber numberWithInt:style], OBJC_ASSOCIATION_RETAIN);
 }
-
 - (int)getIndicatorStyle{
     return [objc_getAssociatedObject(self, &TAG_ACTIVITY_STYLE) intValue];
 }
-
 - (void)addActivityIndicator {
     if (!self.activityIndicator) {
-       
         self.activityIndicator = (UIActivityIndicatorView *)[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
         //wangpeizheng
         self.activityIndicator.image = [UIImage imageNamed:@"PlaecHoderImage.bundle/image_placeholder"];
         self.activityIndicator.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin;
         self.activityIndicator.frame = CGRectMake((self.frame.size.width - 40)/2, (self.frame.size.height - 40)/2, 40, 40);
         [self addSubview:self.activityIndicator];
-        
     }
-    
 }
-
 - (void)removeActivityIndicator {
     if (self.activityIndicator) {
         [self.activityIndicator removeFromSuperview];
         self.activityIndicator = nil;
     }
 }
-
 @end
-
 @implementation UIImageView (WebCacheDeprecated)
-
 - (NSURL *)imageURL {
     return [self sd_imageURL];
 }
-
 - (void)setImageWithURL:(NSURL *)url {
     [self sd_setImageWithURL:url placeholderImage:nil options:0 progress:nil completed:nil];
 }
-
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:0 progress:nil completed:nil];
 }
-
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:options progress:nil completed:nil];
 }
-
 - (void)setImageWithURL:(NSURL *)url completed:(SDWebImageCompletedBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:nil options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         if (completedBlock) {
@@ -253,7 +207,6 @@ static char TAG_ACTIVITY_SHOW;
         }
     }];
 }
-
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder completed:(SDWebImageCompletedBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         if (completedBlock) {
@@ -261,7 +214,6 @@ static char TAG_ACTIVITY_SHOW;
         }
     }];
 }
-
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options completed:(SDWebImageCompletedBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         if (completedBlock) {
@@ -269,7 +221,6 @@ static char TAG_ACTIVITY_SHOW;
         }
     }];
 }
-
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options progress:(SDWebImageDownloaderProgressBlock)progressBlock completed:(SDWebImageCompletedBlock)completedBlock {
     [self sd_setImageWithURL:url placeholderImage:placeholder options:options progress:progressBlock completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         if (completedBlock) {
@@ -277,21 +228,17 @@ static char TAG_ACTIVITY_SHOW;
         }
     }];
 }
-
 - (void)sd_setImageWithPreviousCachedImageWithURL:(NSURL *)url andPlaceholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options progress:(SDWebImageDownloaderProgressBlock)progressBlock completed:(SDWebImageCompletionBlock)completedBlock {
     [self sd_setImageWithPreviousCachedImageWithURL:url placeholderImage:placeholder options:options progress:progressBlock completed:completedBlock];
 }
-
 - (void)cancelCurrentArrayLoad {
     [self sd_cancelCurrentAnimationImagesLoad];
 }
-
 - (void)cancelCurrentImageLoad {
     [self sd_cancelCurrentImageLoad];
 }
-
 - (void)setAnimationImagesWithURLs:(NSArray *)arrayOfURLs {
     [self sd_setAnimationImagesWithURLs:arrayOfURLs];
 }
-
 @end
+
